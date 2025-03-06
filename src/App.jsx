b63498fa647c1f035c27471286c72261b954dc8f
everydayHo <jungwoopage@gmail.com>
2025-03-06 18:44:45 +0900

    공통컴포넌트 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
+import Button from './components/Button';
+import Header from './components/Header';
+// import getEmotionImage from './util/get-emotion-images';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Header title={'Header'} leftChild={<Button text={'left'} />} rightChild={<Button text={'right'} />} />
+			<Button
+				text={'123'}
+				onClick={() => {
+					console.log('버튼');
+				}}
+				type={'NEGATIVE'}
+			/>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/new' element={<New />} />
+				<Route path='/diary/:id' element={<Diary />} />
+				<Route path='*' element={<Notfound />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,5 @@
+const Notfound = () => {
+	return <div>요청하신 페이지가 없습니다.</div>;
+};
+
+export default Notfound;
